5cfa08bbc1b625320efbe232fdcfdc1a758dced4
Matthew Michel <mmichel@tamu.edu>
2022-10-10 20:15:32 -0500

    Retrograde analysis state class and checkmate generator

---
+++ src/retrograde_analysis/state.hpp
@@ -0,0 +1,60 @@
+#ifndef STATE_HPP_
+#define STATE_HPP_
+
+#include <array>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+
+using piece_label_t = unsigned char;
+
+template<::std::size_t FlattenedSz>
+struct BoardState
+{
+  ::std::array<piece_label_t, FlattenedSz> m_board;
+};
+
+// TODO:
+// it is necessary to make copies. all retrograde analysis
+// implementations do this because the algorithm is not a tree 
+// meaning that multiple predecessors may exist. it is necessary to store each state
+
+template<::std::size_t FlattenedSz>
+auto
+generatePredecessors(const BoardState<FlattenedSz>& board);
+
+// TODO:
+// Games like chess have unreachable positions.
+// we can have an is_valid_check position potentially. Not immediately pressing.
+
+// Generate all permutations of up to n-man board positions, enumerating the checkmate states
+// Permutations generator from https://stackoverflow.com/questions/28711797/generating-n-choose-k-permutations-in-c
+template<typename CheckmateEvalFn, ::std::size_t FlattenedSz>
+std::vector<BoardState<FlattenedSz>>
+generateAllCheckmates(const std::vector<piece_label_t>& pieceSet,
+    CheckmateEvalFn checkmateEval)
+{
+  std::vector<BoardState<FlattenedSz>> checkmates;
+  std::array<std::size_t, FlattenedSz> indexPermutations;
+  std::iota(indexPermutations.begin(), indexPermutations.end(), 0);
+  
+  for (auto k_permute = 2; k_permute != pieceSet.size()+1; ++k_permute)
+  {
+    do 
+    {
+      BoardState<FlattenedSz> currentBoard;
+      for (auto i = 0; i != k_permute; ++i)
+        currentBoard.m_board[indexPermutations[i]] = pieceSet[i]; // scatter pieces
+
+      if (checkmateEval(currentBoard))
+        checkmates.push_back(currentBoard);
+
+      std::reverse(indexPermutations.begin() + k_permute, indexPermutations.end());
+    } while (std::next_permutation(indexPermutations.begin(), indexPermutations.end()));
+  }
+
+  return checkmates;
+}
+
+#endif
