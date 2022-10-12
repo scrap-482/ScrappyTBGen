0d4b55545f223a9666b03a2be7a5973c1317e0cf
Matthew Michel <mmichel@tamu.edu>
2022-10-11 21:04:19 -0500

    Add bitboard representation for stretch goal

--- src/retrograde_analysis/state.hpp
+++ src/retrograde_analysis/state.hpp
@@ -9,11 +9,24 @@
 
 using piece_label_t = unsigned char;
 
+#if 1
 template<::std::size_t FlattenedSz>
 struct BoardState
 {
   ::std::array<piece_label_t, FlattenedSz> m_board;
 };
+#else // TODO: Potential future bitboard optimization 
+template<::std::size_t FlattenedSz, ::std::size_t NumUniquePieces>
+struct bb_state
+{
+  using bitboard_t = ::std::bitset<FlattenedSz>;
+
+  ::std::array<bitboard_t, NumUniquePieces> m_piece_bb;
+
+  bitboard_t m_white_bit_board;
+  bitboard_t m_black_bit_board;
+};
+#endif
 
 // TODO:
 // it is necessary to make copies. all retrograde analysis
