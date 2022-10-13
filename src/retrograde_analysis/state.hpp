a3c4eb9f787a1e8db84c5d33109e7c96c5b20af1
Matthew Michel <mmichel@tamu.edu>
2022-10-12 22:04:41 -0500

    Finished combination portion of checkmate enumeration

--- src/retrograde_analysis/state.hpp
+++ src/retrograde_analysis/state.hpp
@@ -13,6 +13,8 @@ using piece_label_t = unsigned char;
 template<::std::size_t FlattenedSz>
 struct BoardState
 {
+  // 1 for white move. 0 for black move
+  //::std::bitset<1> m_player;
   ::std::array<piece_label_t, FlattenedSz> m_board;
 };
 #else // TODO: Potential future bitboard optimization 
@@ -33,6 +35,7 @@ struct bb_state
 // implementations do this because the algorithm is not a tree 
 // meaning that multiple predecessors may exist. it is necessary to store each state
 
+// "unmove" operations matt todo
 template<::std::size_t FlattenedSz>
 auto
 generatePredecessors(const BoardState<FlattenedSz>& board);
@@ -53,12 +56,12 @@ generateCheckmatePermutations(::std::vector<BoardState<FlattenedSz>> checkmates,
   ::std::array<::std::size_t, FlattenedSz> indexPermutations;
   ::std::iota(indexPermutations.begin(), indexPermutations.end(), 0);
   
-  for (auto k_permute = 2; k_permute != pieceSet.size() + 1; ++k_permute)
+  for (::std::size_t k_permute = 2; k_permute != pieceSet.size() + 1; ++k_permute)
   {
     do 
     {
       BoardState<FlattenedSz> currentBoard;
-      for (auto i = 0; i != k_permute; ++i)
+      for (::std::size_t i = 0; i != k_permute; ++i)
         currentBoard.m_board[indexPermutations[i]] = pieceSet[i]; // scatter pieces
 
       if (checkmateEval(currentBoard))
@@ -68,15 +71,32 @@ generateCheckmatePermutations(::std::vector<BoardState<FlattenedSz>> checkmates,
     } while (::std::next_permutation(indexPermutations.begin(), indexPermutations.end()));
   }
 
-  return std::move(checkmates);
+  return ::std::move(checkmates);
 }
 
+// todo: analyze performance tomorrow.
 template<::std::size_t FlattenedSz, ::std::size_t N, typename CheckmateEvalFn>
-generateAllCheckmates(const ::std::vector<piece_label_t>& fullPieceSet, CheckmateEvalFn eval)
+auto generateAllCheckmates(const ::std::vector<piece_label_t>& noKingsPieceset, CheckmateEvalFn eval)
 {
-  //TODO: Finish
   ::std::vector<BoardState<FlattenedSz>> checkmates;
-  auto remaining_N = N - 2; // 2 kings must be present 
+  constexpr auto remaining_N = N - 2; // 2 kings must be present 
+  
+  // https://rosettacode.org/wiki/Combinations#C.2B.2B
+  std::string bitmask(remaining_N, 1); // K leading 1's
+  bitmask.resize(noKingsPieceset.size(), 0); // N-K trailing 0's
+  
+  // generate all C(||noKingsPieceset||, remaining_N) combinations
+  do {  
+    std::vector<piece_label_t> tmpPieceSet(remaining_N);
+    
+    ::std::size_t idx = 0;
+    for (::std::size_t i = 0; i < bitmask.size(); ++i)
+      if (bitmask[i])
+        tmpPieceSet[idx++] = noKingsPieceset[i];
+    checkmates = generateCheckmatePermutations<FlattenedSz>(::std::move(checkmates), noKingsPieceset,
+      eval); // TODO: add kings.
+  } while (::std::prev_permutation(bitmask.begin(), bitmask.end()));
+  
   return checkmates;
 }
 
