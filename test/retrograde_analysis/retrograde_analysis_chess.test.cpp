97aa27b3488f64c79a01d8f2fb555838d66ffac9
mu-504 <84294912+mu-504@users.noreply.github.com>
2022-11-24 12:58:07 -0600

    Generalizing Board Partitioner for Both MPI Procs and OMP Threads (#10)
    * Changed frontier to unordered set to prune duplicate states
    
    * Cleanup of code
    
    * Updates to impl
    
    * changes to test
    
    * Change return time of retrograde analysis impl
    
    * Restored probe code and test case
    
    * Moved board partititioner into state.hpp
    
    * Fixes to partitioner for N >= 4 and generalized to allow invocation by both mpi procs and cpu threads
    
    Co-authored-by: Matthew Michel <mmichel@tamu.edu>

--- test/retrograde_analysis/retrograde_analysis_chess.test.cpp
+++ test/retrograde_analysis/retrograde_analysis_chess.test.cpp
@@ -4,29 +4,64 @@
 
 #include "../../src/rules/chess/interface.h"
 
+#include <cassert>
 #include <iostream>
 
-//void print_win(auto w, int v)
-//{
-//#pragma omp critical
-//  {
-//    std::cout << "found win at v=" << v << std::endl;
-//    std::cout << "Player: " << w.m_player << std::endl;
-//    auto b = w.m_board;
-//    
-//    int count = 0;
-//    for (auto c : b)
-//    {
-//      if (c == '\0')
-//        std::cout << "x ";
-//      else
-//        std::cout << c << " ";
-//      ++count;
-//      if (count % 8 == 0)
-//        std::cout << std::endl;
-//    }
-//  }
-//}
+template<typename BoardType, typename MapType, typename SuccFn>
+auto probe(const BoardType& b, const MapType& m, SuccFn succFn, bool isWinIteration)
+{
+  BoardType g = b;
+  int depthToEnd = 0;
+  auto v = m.at(g);
+  auto print = ChessBoardPrinter();
+  std::vector<BoardType> pathwayToEnd = {g};
+  for (;;)
+  {
+    auto succs = succFn(g);
+    if (succs.size() == 0)
+      break;
+    bool pathFound = false;
+    std::cout << print(g) << std::endl;
+    std::cout << "v=" << v << " succ v= ";
+    for (const auto& succ : succs)
+    {
+      // exploring a draw state
+      if (m.find(succ) == m.end())
+        continue;
+      auto lvlToEnd = m.at(succ);
+      std::cout << lvlToEnd << " ";
+      // levels are doubled up per move
+      if (isWinIteration && lvlToEnd == v - 1) 
+      {
+        //std::cout << lvlToEnd << std::endl;
+        g = succ;
+        pathwayToEnd.push_back(g);
+        ++depthToEnd;
+        v = lvlToEnd;
+        std::cout << lvlToEnd << " " << depthToEnd << std::endl;
+        pathFound = true;
+        break;
+      }
+      else if (!isWinIteration && lvlToEnd == v)
+      {
+        //std::cout << lvlToEnd << std::endl;
+        g = succ;
+        pathwayToEnd.push_back(g);
+        ++depthToEnd;
+        std::cout << lvlToEnd << " " << depthToEnd << std::endl;
+        v = lvlToEnd;
+        pathFound = true;
+        break;
+      }
+    }
+    std::cout << std::endl;
+    assert(pathFound);
+    if (v == 0)
+      break;
+    isWinIteration = !isWinIteration;
+  }
+  return std::make_tuple(depthToEnd, pathwayToEnd);
+}
 
 int main()
 {
@@ -35,31 +70,31 @@ int main()
   auto winCondEvaluator = ChessCheckmateEvaluator();
   auto boardPrinter = ChessBoardPrinter();
 
-  constexpr ::std::size_t N      = 3;
+  constexpr ::std::size_t N      = 4;
   constexpr ::std::size_t ROW_SZ = 8;
   constexpr ::std::size_t COL_SZ = 8;
   
   // TODO: This is hardcoded for now. playout for correctness
-  std::vector<piece_label_t> noRoyaltyPieceset = { 'Q' };
+  std::vector<piece_label_t> noRoyaltyPieceset = { 'q', 'R' };
   std::vector<piece_label_t> royaltyPieceset = { 'k', 'K' };
 
-  std::vector<piece_label_t> fullPieceset = { 'k', 'K', 'q' };
+  std::vector<piece_label_t> fullPieceset = { 'K', 'k', 'q', 'R'};
   
-  //auto checkmates = generateAllCheckmates<64, ChessNPD, N, ROW_SZ, COL_SZ,
-  //  decltype(winCondEvaluator)>(noRoyaltyPieceset, royaltyPieceset, evaluator);
-  
-  auto checkmates = generateConfigCheckmates<64, ChessNPD, N, ROW_SZ, COL_SZ,
+  //auto checkmates = generateConfigCheckmates<64, ChessNPD, N, ROW_SZ, COL_SZ,
+  //     decltype(winCondEvaluator)>(fullPieceset, winCondEvaluator);
+
+  auto checkmates = generateParallelConfigCheckmates<64, ChessNPD, N, ROW_SZ, COL_SZ,
        decltype(winCondEvaluator)>(fullPieceset, winCondEvaluator);
+
+  std::cout << checkmates.size() << std::endl;
   
   std::cout << "done with checkmates" << std::endl;
 
-  auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<64, ChessNPD, 3, ROW_SZ, 
-    COL_SZ, decltype(fwdMoveGenerator), decltype(revMoveGenerator)>(::std::move(checkmates),
-      fwdMoveGenerator, revMoveGenerator);
-  
-  std::cout << wins.size() << " " << losses.size() << std::endl;
-  //for (auto& l : checkmates)
-  //  print_win(l, 0);
+  //auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<64, ChessNPD, N, ROW_SZ, 
+  //  COL_SZ, decltype(fwdMoveGenerator), decltype(revMoveGenerator)>(::std::move(checkmates),
+  //    fwdMoveGenerator, revMoveGenerator);
 
+  //std::cout << wins.size() << " " << losses.size() << std::endl;
+  
   return 0;
 }
