995a9887506b0889a3c0a9837e68dafd43e9cb4f
mu-504 <84294912+mu-504@users.noreply.github.com>
2022-11-29 08:43:52 -0600

    User Input Checking, Probing, and Board Evaluation Functor in main.cpp (#15)
    * CL arg error checking, board validity functor, and probe code
    
    * Fully functioning probing in main.cpp
    
    * Fixed error checking on binary cl input
    
    Co-authored-by: Matthew Michel <mmichel@tamu.edu>

--- src/retrograde_analysis/main.cpp
+++ src/retrograde_analysis/main.cpp
@@ -1,8 +1,15 @@
+// TODO:
+// MPI stuff
+// Board printing
 #include "retrograde_analysis.hpp"
 #include <cassert>
 #include <iostream>
 #include <algorithm>
 #include <stdio.h>
+
+#include "probe.hpp"
+
+// preprocessor checks and defs for optional fields
 #ifndef HZ_SYM_EVALUATOR
 #  define HZ_SYM_EVALUATOR false_fn
 #endif
@@ -16,79 +23,125 @@
 auto readClArgs(int argc, char* argv[], 
   const std::vector<piece_label_t>& royaltyPieceset)
 {
-//does not account for commas
-std::string fullset = argv[1]; //see if argc > 1???
-
-std::vector<piece_label_t> userset;
-for(int i = 0; i < fullset.length(); i++){
-  userset.push_back(fullset.at(i));
-}
-//sort royalty pieces first (assuming we have royalty_set)
-std::sort(userset.begin(), userset.end(), 
-  [&royaltyPieceset = std::as_const(royaltyPieceset)]
-  (const char fst, const char snd)
+  if (argc == 1)
   {
-    // fst is royalty, let us put it first
-    if (std::find(royaltyPieceset.begin(), royaltyPieceset.end(), fst) 
-      != royaltyPieceset.end())
-      return true;
-    // snd is royalty (and fst is not), so we put snd before fst
-    else if (std::find(royaltyPieceset.begin(), royaltyPieceset.end(),
-      snd) != royaltyPieceset.end())
-      return false;
-    return true; // neither piece is royalty, so we don't care
-  });
+    std::cerr << "ERROR: Please enter a pieceset confiuration" << std::endl;
+    assert(false);
+  }
+  //does not account for commas
+  std::string fullset = argv[1]; //see if argc > 1???
 
-return userset;
+  std::vector<piece_label_t> userset;
+  for(int i = 0; i < fullset.length(); i++){
+    userset.push_back(fullset.at(i));
+  }
+  //sort royalty pieces first (assuming we have royalty_set)
+  std::sort(userset.begin(), userset.end(), 
+    [&royaltyPieceset = std::as_const(royaltyPieceset)]
+    (const char fst, const char snd)
+    {
+      // fst is royalty, let us put it first
+      if (std::find(royaltyPieceset.begin(), royaltyPieceset.end(), fst) 
+        != royaltyPieceset.end())
+        return true;
+      // snd is royalty (and fst is not), so we put snd before fst
+      else if (std::find(royaltyPieceset.begin(), royaltyPieceset.end(),
+        snd) != royaltyPieceset.end())
+        return false;
+      return true; // neither piece is royalty, so we don't care
+    });
+
+  return userset;
 }
 
 // look out for clashing of macros with actual function names
 int main(int argc, char* argv[])
 {
-std::vector<piece_label_t> noRoyaltyPieceset = NO_ROYALTY_PIECESET;
-std::vector<piece_label_t> royaltyPieceset = ROYALTY_PIECESET;
+  std::vector<piece_label_t> noRoyaltyPieceset = NO_ROYALTY_PIECESET;
+  std::vector<piece_label_t> royaltyPieceset = ROYALTY_PIECESET;
 
-std::vector<piece_label_t> fullPieceset = readClArgs(argc, argv, royaltyPieceset);
+  std::vector<piece_label_t> fullPieceset = readClArgs(argc, argv, royaltyPieceset);
 
-#if defined ROW_SZ && defined COL_SZ && defined N_MAN
-constexpr std::size_t FLATTENED_SZ = ROW_SZ * COL_SZ;
+#if defined ROW_SZ && defined COL_SZ && defined N_MAN && defined FORWARD_MOVE_GENERATOR \
+  && defined REVERSE_MOVE_GENERATOR && defined WIN_COND_EVALUATOR
+  constexpr std::size_t FLATTENED_SZ = ROW_SZ * COL_SZ;
 
-FORWARD_MOVE_GENERATOR forward;
-REVERSE_MOVE_GENERATOR reverse;
+  FORWARD_MOVE_GENERATOR forward;
+  REVERSE_MOVE_GENERATOR reverse;
 
-HZ_SYM_EVALUATOR hzSymmetryCheck;
-VT_SYM_EVALUATOR vtSymmetryCheck;
-IS_VALID_BOARD_FN isValidBoardFn;
-WIN_COND_EVALUATOR winEval;
+  HZ_SYM_EVALUATOR hzSymmetryCheck;
+  VT_SYM_EVALUATOR vtSymmetryCheck;
+  IS_VALID_BOARD_FN isValidBoardFn;
+  WIN_COND_EVALUATOR winEval;
 
-// worry about this later - most users will have a custom process due to how these
-// are scheduled
+  // worry about this later - most users will have a custom process due to how these
+  // are scheduled
 #ifndef MULTI_NODE
-auto checkmates = generateParallelConfigCheckmates<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, COL_SZ,
-       decltype(winEval)>(fullPieceset, winEval);
+  auto checkmates = generateParallelConfigCheckmates<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, COL_SZ,
+         decltype(winEval), decltype(isValidBoardFn)>(fullPieceset, winEval, isValidBoardFn);
 
-auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, 
-    COL_SZ, decltype(forward), decltype(reverse)>(::std::move(checkmates),
-    forward, reverse);
+  auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, 
+      COL_SZ, decltype(forward), decltype(reverse)>(::std::move(checkmates),
+      forward, reverse);
 
-std::cout << wins.size() << " " << losses.size() << std::endl;
+  std::cout << "Number of wins: " << wins.size() << " Number of losses: " << losses.size() << std::endl; 
+  // todo: adjust this to be generic
+  auto boardPrinter = ChessBoardPrinter();
+
+  //BoardState<64, ChessNPD> longest3ManDTM
+  //{
+  //  true,
+  //  {
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','\0','\0','\0','\0','\0','\0',
+  //    '\0','\0','r','K','\0','\0','\0','\0',
+  //    '\0','k','\0','\0','\0','\0','\0','\0',
+  //  },
+  //  {-1}
+  //};
+  BoardState<64, ChessNPD> longest3ManDTM
+  {
+    true,
+    {
+      '\0','\0','\0','\0','\0','\0','\0','\0',
+      '\0','\0','\0','\0','\0','\0','\0','\0',
+      '\0','\0','\0','\0','\0','\0','\0','\0',
+      '\0','\0','\0','\0','\0','\0','\0','\0',
+      '\0','\0','\0','\0', 'K','\0','\0','\0',
+      '\0','\0','\0','\0','\0','\0','\0','\0',
+      '\0', 'q','\0','\0','\0','\0','\0','\0',
+      'k', '\0','\0','\0','\0','\0','\0','\0',
+    },
+    {-1}
+  };
+
+  
+  bool iteration;
+  if (wins.find(longest3ManDTM) != wins.end())
+    iteration = true;
+  else if (losses.find(longest3ManDTM) != losses.end())
+    iteration = false;
+  else
+    assert(false);
+    
+  auto [depthToEnd, path] = probe(longest3ManDTM, dtm, forward, iteration, boardPrinter);
+  std::cout << "Number of moves until the end: " << depthToEnd << std::endl;
 
 #else
-//auto checkmates = generateConfigCheckmates<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, COL_SZ,
-       //decltype(winEval)>(fullPieceset, winEval);
+  auto checkmates = generateParallelConfigCheckmates<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, COL_SZ,
+         decltype(winEval)>(fullPieceset, winEval);
 
-auto checkmates = generateParallelConfigCheckmates<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, COL_SZ,
-       decltype(winEval)>(fullPieceset, winEval);
+  auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, 
+      COL_SZ, decltype(forward), decltype(reverse)>(::std::move(checkmates), forward, reverse);
 
-auto [wins, losses, dtm] = retrogradeAnalysisBaseImpl<FLATTENED_SZ, NON_PLACEMENT_DATATYPE, N_MAN, ROW_SZ, 
-    COL_SZ, decltype(forward), decltype(reverse)>(::std::move(checkmates),
-    forward, reverse);
-
-std::cout << wins.size() << " " << losses.size() << std::endl;
-
+  std::cout << "Number of wins: " << wins.size() << " Number of losses: " << losses.size() << std::endl; 
 #endif
 #else
-std::cerr << "ERROR: Invalid configuration file" << std::endl;
-assert(false);
+  std::cerr << "ERROR: Invalid configuration file" << std::endl;
+  assert(false);
 #endif
 }
---
+++ src/retrograde_analysis/probe.hpp
@@ -0,0 +1,52 @@
+#ifndef PROBE_HPP_
+
+#include <vector>
+#include "state.hpp"
+#include <iostream>
+
+template<typename BoardType, typename MapType, typename SuccFn,
+  typename BoardPrinter>
+auto probe(const BoardType& b, const MapType& m, SuccFn succFn, bool isWinIteration,
+    BoardPrinter print)
+{
+  BoardType g = b;
+  int depthToEnd = 0;
+  auto v = m.at(g);
+  std::vector<BoardType> pathwayToEnd = {g};
+  for (;;)
+  {
+    auto succs = succFn(g);
+    if (succs.size() == 0)
+      break;
+    std::cout << print(g) << std::endl;
+    for (const auto& succ : succs)
+    {
+      // exploring a draw state
+      if (m.find(succ) == m.end())
+        continue;
+      auto lvlToEnd = m.at(succ);
+      // levels are doubled up per move
+      if (isWinIteration && lvlToEnd == v - 1) 
+      {
+        g = succ;
+        pathwayToEnd.push_back(g);
+        ++depthToEnd;
+        v = lvlToEnd;
+        break;
+      }
+      else if (!isWinIteration && lvlToEnd == v)
+      {
+        g = succ;
+        pathwayToEnd.push_back(g);
+        ++depthToEnd;
+        v = lvlToEnd;
+        break;
+      }
+    }
+    if (v == 0)
+      return std::make_tuple(depthToEnd, pathwayToEnd);
+    isWinIteration = !isWinIteration;
+  }
+  return std::make_tuple(depthToEnd, pathwayToEnd);
+}
+#endif
