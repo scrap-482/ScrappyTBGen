3713d261085c886e11ef8080bf593502813d2524
Jose Carrillo <34354911+jose01carrillo30@users.noreply.github.com>
2022-12-01 00:01:48 -0600

    pulled directedJumpPMO out of chess_pmo

--- src/core/pmo_instantiable.hpp
+++ src/core/pmo_instantiable.hpp
@@ -12,7 +12,7 @@
 // FS = FlattenedSize
 // NPDT = NonPlacementDataType
 // CT = CoordsType
-// PTC = "Piece Type Count" AKA NumPieceTypes // TODO: bad naming, refactor?
+// PTC = "Piece Type Count" AKA NumPieceTypes
 
 
 template<::std::size_t FS, typename NPDT, typename CT, ::std::size_t PTC>
@@ -47,9 +47,8 @@ public:
 
                 newState.m_board.at(pieceEndPos.flatten()) = newState.m_board.at(piecePos.flatten());
                 newState.m_board.at(piecePos.flatten()) = '\0';
-                // Any time we make a move, invert turn and reset en-passant. //TODO: smells bad doing this here, separate this functionality somehow.
+                // Any time we make a move //TODO: smells bad doing this here, separate this functionality somehow.
                 newState.m_player = !newState.m_player; 
-                // TODO: reset en passant info
 
                 resultStates.push_back(newState);
                 resultDisplacements.push_back(displacementMultiplier * moveOffset);
@@ -87,7 +86,7 @@ public:
             }
         }
         auto unmoves = std::make_pair(std::move(resultStates), std::move(resultDisplacements));
-        addUncaptures<FS, NPDT, CT, PTC>(b, piecePos, unmoves); //FIXME: hack
+        addUncaptures<FS, NPDT, CT, PTC>(b, piecePos, unmoves);
         // then just add uncaptures
         return unmoves;
     }
@@ -97,5 +96,147 @@ public:
 
 };
 
+// JumpPMO but moveOffsets are negated for black.
+template<::std::size_t FS, typename NPDT, typename CT, ::std::size_t PTC>
+class DirectedJumpPMO : public ModdablePMO<FS, NPDT, CT, PTC> {
+public:
+    // The possible displacements for a white piece, stored as flattened coordinates.
+    // ASSUMPTION: can just be negated for black pieces. Sufficient if rectangular board and moves is horizontally symmetric.
+    std::vector<CT> moveOffsets;
 
+    // Parallel to moveOffsets, each vector for a moveOffset specifies which tiles must be empty so this piece can 'pass through' it (e.g. Xiangqi horse, double-jumping pawn).
+    std::vector<std::vector<CT>> obstructOffsets;
+
+    virtual ::std::pair<::std::vector<BoardState<FS, NPDT>>, ::std::vector<CT>> 
+    getModdableMovesWithDisplacement(const BoardState<FS, NPDT>& b,  CT piecePos) const override {
+        std::vector<BoardState<FS, NPDT>> resultStates;
+        // parallel to resultStates, describes the displacement of the moving piece's coords
+        std::vector<CT> resultDisplacements;
+
+        for (size_t offsetIndex = 0; offsetIndex < moveOffsets.size(); ++offsetIndex) {
+            auto moveOffset = moveOffsets.at(offsetIndex);
+            // this is just the piece we are moving
+            auto movedFromContents = b.m_board.at(piecePos.flatten());
+            // negate if black
+            if (!isWhite(movedFromContents)) moveOffset *= -1;
+
+            CT pieceEndPos = piecePos + moveOffset;
+            if (!inBounds(pieceEndPos)) continue;
+
+            bool obstructed = false;
+            for (auto obstructOffset : obstructOffsets.at(offsetIndex)) {
+                // negate if black
+                if (!isWhite(movedFromContents)) obstructOffset *= -1;
+
+                auto obstrContents = b.m_board.at((piecePos + obstructOffset).flatten());
+                if (obstrContents != '\0') {
+                    obstructed = true;
+                    break;
+                }
+            }
+            if (obstructed) continue;
+
+            auto movedToContents = b.m_board.at(pieceEndPos.flatten());
+
+            if (!isEmpty(movedToContents)) {
+                // if the player-to-move's color is the same as the piece we are moving to, not allowed.
+                if (isWhite(movedToContents) == b.m_player) continue;
+                // Otherwise, this is a capture
+            }
+            BoardState<FS, NPDT> newState(b); // copy state.
+
+            newState.m_board.at(pieceEndPos.flatten()) = newState.m_board.at(piecePos.flatten());
+            newState.m_board.at(piecePos.flatten()) = '\0';
+            // Any time we make a move, invert turn //TODO: smells bad doing this here, separate this functionality somehow.
+            newState.m_player = !newState.m_player; 
+
+            resultStates.push_back(newState);
+            resultDisplacements.push_back(moveOffset);
+        }
+        
+        return std::make_pair(std::move(resultStates), std::move(resultDisplacements));
+    }
+
+    virtual ::std::pair<::std::vector<BoardState<FS, NPDT>>, ::std::vector<CT>> 
+    getModdableUnmovesWithDisplacement(const BoardState<FS, NPDT>& b,  CT piecePos) const override {
+        std::vector<BoardState<FS, NPDT>> resultStates;
+        // parallel to resultStates, describes the displacement of the moving piece's coords
+        std::vector<CT> resultDisplacements;
+
+        for (size_t offsetIndex = 0; offsetIndex < moveOffsets.size(); ++offsetIndex) {
+            auto moveOffset = moveOffsets.at(offsetIndex);
+            // this is just the piece we are moving
+            auto movedFromContents = b.m_board.at(piecePos.flatten());
+            // negate since this is UNmove, then negate again if black
+            if (isWhite(movedFromContents)) moveOffset *= -1;
+
+            CT pieceEndPos = piecePos + moveOffset;
+            if (!inBounds(pieceEndPos)) continue;
+
+            bool obstructed = false;
+            for (auto obstructOffset : obstructOffsets.at(offsetIndex)) {
+                // negate since this is UNmove, then negate again if black
+                if (isWhite(movedFromContents)) obstructOffset *= -1;
+
+                auto obstrContents = b.m_board.at((piecePos + obstructOffset).flatten());
+                if (obstrContents != '\0') {
+                    obstructed = true;
+                    break;
+                }
+            }
+            if (obstructed) continue;
+
+            auto movedToContents = b.m_board.at(pieceEndPos.flatten());
+            // ASSUMPTION: moves leave behind empty tile
+            if (!isEmpty(movedToContents)) continue; // TODO: consider abstracting fwd and rev move to have less repetition
+            BoardState<FS, NPDT> newState(b); // copy state.
+
+            newState.m_board.at(pieceEndPos.flatten()) = newState.m_board.at(piecePos.flatten());
+            newState.m_board.at(piecePos.flatten()) = '\0';
+            // Any time we make an unmove, invert turn.
+            newState.m_player = !newState.m_player; 
+
+            resultStates.push_back(newState);
+            resultDisplacements.push_back(moveOffset);
+        }
+        
+        auto unmoves = std::make_pair(std::move(resultStates), std::move(resultDisplacements));
+        // non-capture unmoves are same as forward moves, but with opposite person playing
+        addUncaptures<FS, NPDT, CT, PTC>(b, piecePos, unmoves);
+        // then just add uncaptures
+        return unmoves;
+    }
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets) 
+    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<CT>()) { }
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets, ::std::vector<std::vector<CT>> _obstructOffsets) 
+    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets) { }
+
+    using PMOPreModList_ = ::std::vector<const PMOPreMod<FS, NPDT, CT>*>;
+    using PMOPostModList_ = ::std::vector<const PMOPostMod<FS, NPDT, CT>*>;
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets
+        , PMOPreModList_ preFwdMods, PMOPostModList_ postFwdMods, PMOPreModList_ preBwdMods, PMOPostModList_ postBwdMods) 
+    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<CT>())
+        , ModdablePMO<FS, NPDT, CT, PTC>(preFwdMods, postFwdMods, preBwdMods, postBwdMods) { }
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets
+        , PMOPreModList_ preFwdMods, PMOPostModList_ postFwdMods, PMOPreModList_ preBwdMods, PMOPostModList_ postBwdMods
+        , PMOPreModList_ preUnpromotionMods, PMOPostModList_ postUnpromotionMods) 
+    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<CT>())
+        , ModdablePMO<FS, NPDT, CT, PTC>(preFwdMods, postFwdMods, preBwdMods, postBwdMods, preUnpromotionMods, postUnpromotionMods) { }
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets, ::std::vector<std::vector<CT>> _obstructOffsets
+        , PMOPreModList_ preFwdMods, PMOPostModList_ postFwdMods, PMOPreModList_ preBwdMods, PMOPostModList_ postBwdMods) 
+    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets)
+        , ModdablePMO<FS, NPDT, CT, PTC>(preFwdMods, postFwdMods, preBwdMods, postBwdMods) { }
+
+    DirectedJumpPMO(::std::vector<CT> _moveOffsets, ::std::vector<std::vector<CT>> _obstructOffsets
+        , PMOPreModList_ preFwdMods, PMOPostModList_ postFwdMods, PMOPreModList_ preBwdMods, PMOPostModList_ postBwdMods
+        , PMOPreModList_ preUnpromotionMods, PMOPostModList_ postUnpromotionMods)
+    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets)
+        , ModdablePMO<FS, NPDT, CT, PTC>(preFwdMods, postFwdMods, preBwdMods, postBwdMods, preUnpromotionMods, postUnpromotionMods) { }
+
+};
 #endif
--- src/rules/chess/chess_pmo.h
+++ src/rules/chess/chess_pmo.h
@@ -28,51 +28,7 @@ using ChessDirRegionMod = DirectedRegionPMOPreMod<64, ChessNPD, Coords>;
 using ChessPromotionFwdPostMod = RegionalForcedSinglePromotionPMOPostMod<64, ChessNPD, Coords>;
 
 using ChessSlidePMO = SlidePMO<64, ChessNPD, Coords, NUM_PIECE_TYPES>;
-
-// JumpPMO but moveOffsets are negated for black.
-class DirectedJumpPMO : public ChessModdablePMO {
-public:
-    // The possible displacements for a white piece, stored as flattened coordinates.
-    // ASSUMPTION: can just be negated for black pieces. Sufficient if rectangular board and moves is horizontally symmetric.
-    std::vector<Coords> moveOffsets;
-
-    // Parallel to moveOffsets, each vector for a moveOffset specifies which tiles must be empty so this piece can 'pass through' it (e.g. Xiangqi horse, double-jumping pawn).
-    std::vector<std::vector<Coords>> obstructOffsets;
-
-    virtual ::std::pair<::std::vector<ChessBoardState>, ::std::vector<Coords>> 
-    getModdableMovesWithDisplacement(const ChessBoardState& b,  Coords piecePos) const override;
-    virtual ::std::pair<::std::vector<ChessBoardState>, ::std::vector<Coords>> 
-    getModdableUnmovesWithDisplacement(const ChessBoardState& b,  Coords piecePos) const override;
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets) 
-    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<Coords>()) { }
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets, ::std::vector<std::vector<Coords>> _obstructOffsets) 
-    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets) { }
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets
-        , ChessPMOPreModList preFwdMods, ChessPMOPostModList postFwdMods, ChessPMOPreModList preBwdMods, ChessPMOPostModList postBwdMods) 
-    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<Coords>())
-        , ChessModdablePMO(preFwdMods, postFwdMods, preBwdMods, postBwdMods) { }
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets
-        , ChessPMOPreModList preFwdMods, ChessPMOPostModList postFwdMods, ChessPMOPreModList preBwdMods, ChessPMOPostModList postBwdMods
-        , ChessPMOPreModList preUnpromotionMods, ChessPMOPostModList postUnpromotionMods) 
-    : moveOffsets(_moveOffsets), obstructOffsets(_moveOffsets.size(), std::vector<Coords>())
-        , ChessModdablePMO(preFwdMods, postFwdMods, preBwdMods, postBwdMods, preUnpromotionMods, postUnpromotionMods) { }
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets, ::std::vector<std::vector<Coords>> _obstructOffsets
-        , ChessPMOPreModList preFwdMods, ChessPMOPostModList postFwdMods, ChessPMOPreModList preBwdMods, ChessPMOPostModList postBwdMods) 
-    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets)
-        , ChessModdablePMO(preFwdMods, postFwdMods, preBwdMods, postBwdMods) { }
-
-    DirectedJumpPMO(::std::vector<Coords> _moveOffsets, ::std::vector<std::vector<Coords>> _obstructOffsets
-        , ChessPMOPreModList preFwdMods, ChessPMOPostModList postFwdMods, ChessPMOPreModList preBwdMods, ChessPMOPostModList postBwdMods
-        , ChessPMOPreModList preUnpromotionMods, ChessPMOPostModList postUnpromotionMods)
-    : moveOffsets(_moveOffsets), obstructOffsets(_obstructOffsets)
-        , ChessModdablePMO(preFwdMods, postFwdMods, preBwdMods, postBwdMods, preUnpromotionMods, postUnpromotionMods) { }
-
-};
+using ChessDirectedJumpPMO = DirectedJumpPMO<64, ChessNPD, Coords, NUM_PIECE_TYPES>;
 
 namespace ChessPMOs {
     // need these functions for pawns. //TODO: can these be lambdas or something nicer looking? Idk
@@ -115,21 +71,21 @@ namespace ChessPMOs {
     const ChessPMOPreModList  pawnDJPreBwdMods = {&startOnFourthRank};
     const ChessPMOPostModList pawnDJPostBwdMods = {&bwdCaptureProhibitedMod};
 
-    const auto pawnForward = DirectedJumpPMO(std::vector<Coords>{{0, 1}}
+    const auto pawnForward = ChessDirectedJumpPMO(std::vector<Coords>{{0, 1}}
         , noPreMods, pawnForwardPostFwdMods, noPreMods, pawnForwardPostBwdMods, pawnUnpromotionPreMods, noPostMods);
-    const auto pawnAttack = DirectedJumpPMO(std::vector<Coords>{{-1, 1}, {1, 1}}
+    const auto pawnAttack = ChessDirectedJumpPMO(std::vector<Coords>{{-1, 1}, {1, 1}}
         , noPreMods, pawnAttackPostFwdMods, noPreMods, pawnAttackPostBwdMods, pawnUnpromotionPreMods, noPostMods);
-    const auto pawnDouble = DirectedJumpPMO(std::vector<Coords>{{0, 2}}, std::vector<std::vector<Coords>>{{{0, 1}}}
+    const auto pawnDouble = ChessDirectedJumpPMO(std::vector<Coords>{{0, 2}}, std::vector<std::vector<Coords>>{{{0, 1}}}
         , pawnDJPreFwdMods, pawnDJPostFwdMods, pawnDJPreBwdMods, pawnDJPostBwdMods, pawnUnpromotionPreMods, noPostMods);
     const auto orthoSlide = ChessSlidePMO(std::vector<Coords>{{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
     const auto diagSlide = ChessSlidePMO(std::vector<Coords>{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}});
-    const auto knightLeap = DirectedJumpPMO(std::vector<Coords>{
+    const auto knightLeap = ChessDirectedJumpPMO(std::vector<Coords>{
         {-2, -1}, {-2, 1}, {2, -1}, {2, 1},
         {-1, -2}, {-1, 2}, {1, -2}, {1, 2}});
-    const auto kingMove = DirectedJumpPMO(std::vector<Coords>{
+    const auto kingMove = ChessDirectedJumpPMO(std::vector<Coords>{
         {-1, 0}, {1, 0}, {0, -1}, {0, 1},
         {-1, -1}, {-1, 1}, {1, -1}, {1, 1}});
-    const auto noopMove = DirectedJumpPMO(std::vector<Coords>{});
+    const auto noopMove = ChessDirectedJumpPMO(std::vector<Coords>{});
 
     const size_t pawnPMOsCount = 3;
     const ChessPMO* const pawnPMOs [pawnPMOsCount] = {&pawnForward, &pawnAttack, &pawnDouble};
