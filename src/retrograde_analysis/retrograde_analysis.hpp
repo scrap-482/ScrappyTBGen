11d628c9f2b41113025bac9473643ab555192543
Matthew Michel <mmichel@tamu.edu>
2022-12-11 20:40:37 -0600

    Add gplv3 licensing

--- src/retrograde_analysis/retrograde_analysis.hpp
+++ src/retrograde_analysis/retrograde_analysis.hpp
@@ -1,4 +1,20 @@
 /*
+* Copyright 2022 SCRAP
+*
+* This file is part of Scrappy Tablebase Generator.
+* 
+* Scrappy Tablebase Generator is free software: you can redistribute it and/or modify it under the terms 
+* of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+* or (at your option) any later version.
+* 
+* Scrappy Tablebase Generator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with Scrappy Tablebase Generator. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+/*
  * Wrappers around the base implementations of retrograde analysis
  * are provided within this header file. They are not currently used
  * within main.cpp but this may be used for convenience by users in the future
--- src/rules/capablanca/pmo_specs.hpp
+++ src/rules/capablanca/pmo_specs.hpp
@@ -1,3 +1,19 @@
+/*
+* Copyright 2022 SCRAP
+*
+* This file is part of Scrappy Tablebase Generator.
+* 
+* Scrappy Tablebase Generator is free software: you can redistribute it and/or modify it under the terms 
+* of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+* or (at your option) any later version.
+* 
+* Scrappy Tablebase Generator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with Scrappy Tablebase Generator. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef CAPABLANCA_PMO_H_
 #define CAPABLANCA_PMO_H_
 
